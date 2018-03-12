e01bf146f09a2b3df59c45a0e8d17a0c78d283c5
Jessica L.T. Martin <apersonofnote@gmail.com>
2018-03-12 14:54:53 -0700

    I2: uses HTML with an ERB template

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,6 +4,6 @@ require 'sinatra/reloader'
 
 x = rand(100);
 get '/' do
-
-	"The secret number is #{x}";	
+	#render erb template called index. Create local variable for x that is equal to x from this file.
+	erb :index, :locals => {:x => x}
 end
