32f0e3412598bac691af5eb3c9b6a658dd1067dd
Jessica L.T. Martin <apersonofnote@gmail.com>
2018-03-12 15:39:58 -0700

    Game technically functional

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-
 x = rand(100);
+SECRET_NUMBER = x;
+
+def check_guess(guess)
+	if guess > SECRET_NUMBER+20
+		return "Way too high!"
+	elsif guess > SECRET_NUMBER
+		return "Too high."
+	elsif guess < SECRET_NUMBER-20
+		return "Way too low!"
+	elsif guess < SECRET_NUMBER
+		return "Too low"
+	else return "You got it! The secret number is #{SECRET_NUMBER}"
+	end
+end
+
+def display_guess(guess)
+	return "#{guess}"
+end
+
+
 get '/' do
+	guess = params["guess"].to_i
+	show = display_guess(guess)
+	message = check_guess(guess)
 	#render erb template called index. Create local variable for x that is equal to x from this file.
-	erb :index, :locals => {:x => x}
+	erb :index, :locals => {:x => x, :message => message, :show => show }
 end
